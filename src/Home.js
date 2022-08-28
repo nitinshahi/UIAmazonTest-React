1b510da92ccb7677cd418bd57f13ed5235e07ba1
nitin shahi <shahinitin83@gmail.com>
2022-08-28 17:22:34 +0545

    Second  commit

---
+++ src/Home.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './Home.css'
+
+function Home() {
+  return (
+    <div className='home'>
+      <div className='home__container'>
+        <img className='home__image'
+        src='https://tshirtnepal.com/images/slider_images/sweatshirt.jpg' alt='Error'/>
+      
+        <div className='home__row'>
+          {/* product*/}
+          {/* product*/}
+          {/* product*/}
+          {/* product*/}
+          {/* product*/}
+        </div>
+
+        <div className='home__row'>
+          {/* product*/}
+          {/* product*/}
+          {/* product*/}
+          </div>
+
+          <div className='home__row'>
+          {/* product*/}
+          </div>
+    
+
+      </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDGskEUV9_Ecjl54nxxWJraRUnQHnkw244",
+    authDomain: "astore-9f7c8.firebaseapp.com",
+    projectId: "astore-9f7c8",
+    storageBucket: "astore-9f7c8.appspot.com",
+    messagingSenderId: "417844513971",
+    appId: "1:417844513971:web:c705418db73a19adcb34c6"
+  };
